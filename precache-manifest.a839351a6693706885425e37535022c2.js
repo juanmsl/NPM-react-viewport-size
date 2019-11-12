ca80cb6b57d64a3a852b3aa64fe4dac118107a00
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2019-11-12 16:08:02 -0500

    Updates

---
+++ precache-manifest.a839351a6693706885425e37535022c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bb5e156ad7a8938784bb220fea84fb",
+    "url": "/react-viewport-size/index.html"
+  },
+  {
+    "revision": "21663c12ead148aa0d4a",
+    "url": "/react-viewport-size/static/css/main.5499ef94.chunk.css"
+  },
+  {
+    "revision": "d2129e2226cef71ffac6",
+    "url": "/react-viewport-size/static/js/2.409ba596.chunk.js"
+  },
+  {
+    "revision": "21663c12ead148aa0d4a",
+    "url": "/react-viewport-size/static/js/main.088fa5f7.chunk.js"
+  },
+  {
+    "revision": "a4ba43a625956db21d0d",
+    "url": "/react-viewport-size/static/js/runtime-main.ed0a76fe.js"
+  }
+]);
