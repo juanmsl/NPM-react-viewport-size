99c9125e596276653d74760ec67ebf0311c9b8ae
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2019-11-12 16:05:33 -0500

    Updates

---
+++ precache-manifest.cf72b0dcfc03a6f1e04c919fba73ac38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f016f0ccb21e05b7657d0bae2a9d2580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21663c12ead148aa0d4a",
+    "url": "/static/css/main.5499ef94.chunk.css"
+  },
+  {
+    "revision": "d2129e2226cef71ffac6",
+    "url": "/static/js/2.409ba596.chunk.js"
+  },
+  {
+    "revision": "21663c12ead148aa0d4a",
+    "url": "/static/js/main.088fa5f7.chunk.js"
+  },
+  {
+    "revision": "2da398b086e20f9ef8c6",
+    "url": "/static/js/runtime-main.dd75f15b.js"
+  }
+]);
